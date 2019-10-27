778eddb5c30f3edc97d5bd8ed202f75c34da8ed3
Joseph Burks <joe_burks_3@yahoo.com>
2019-10-27 22:41:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,10 @@ require "yaml"
 
 def load_library(file_path)
   emots = YAML.load_file(file_path)
-  new_hash = {}
+  new_hash = {
+    'get_meaning' => {};
+    'get_emoticon' => {}
+  }
 end
 
 def get_japanese_emoticon
